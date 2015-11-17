375620e30f96c04d2e9a439cb34d32f297a529ce
YongkyunLee <ykhl1itj@naver.com>
2015-11-16 23:55:54 -0800

    Improvements
    *Makefile:
    -added 'all', which compiles both 1d_cell_auto & 1d_cell_auto_pointer
    *1d_cell_auto.c
    -added memory leakage check
    -added code to free memory for boundary conditions
    *1d_cell_auto_pointer.c
    -used pointer instead of array to do the same work as 1d_cell_auto.c

--- Assignment5_1d_cell_automata/1d_cell_auto_pointer.c
+++ Assignment5_1d_cell_automata/1d_cell_auto_pointer.c
@@ -1,5 +1,5 @@
 /* This program simulates a 1 dimensional cellular automation
-using array method.
+using pointer arithmetic.
 An array composed of 0 and 1 will be generated according to the rules:
 1. If the current cell is empty, and one of the two adjacent
 cells is full (but not both), set the current cell to be full.
@@ -55,6 +55,8 @@ int *update(int arr[], int num_element)
 {
   int i;
   int *next_arr; /* array of next generation cells */
+  int *cell1, *cell2, *cell3; /* pointer of the input array*/
+  int *next_cell; /* pointer of the array writing to */
   /* initialize array to have num_element elements */
   next_arr = (int *)calloc(num_element, sizeof(int));
   /* Check that the calloc call succeeded */
@@ -63,25 +65,43 @@ int *update(int arr[], int num_element)
     fprintf(stderr, "Error! Memory allocation failed.\n");
     exit(1);
   }
-  next_arr[0] = 0;   /* The first and the last cells are always empty. */
-  next_arr[num_element - 1] = 0;
+  /* initialize pointers */
+  cell1 = arr;
+  cell2 = cell1++;
+  cell3 = cell2++;
+  next_cell = next_arr;
+  *next_cell = 0; /* The first cell is always empty. */
+  next_cell++; /* Move pointer one space to the right */
   /* make next generation array according to the rules:
   Full(1) if one of adjacent cells is full. Empty(0) otherwise */
   for (i = 1; i < num_element - 1; i++)
   {
-    if (arr[i] == 0 && arr[i - 1] == 1 && arr[i + 1] == 0)
+    if (*cell1 == 1 && *cell2 == 0 && *cell3 == 0)
     {
-      next_arr[i] = 1;
+      *next_cell = 1;
+      cell1++;
+      cell2++;
+      cell3++;
+      next_cell++;
     }
-    else if (arr[i] == 0  && arr[i - 1] == 0 && arr[i + 1]==1)
+    else if (*cell1 == 0  && *cell2 == 0 && *cell3 == 1)
     {
-      next_arr[i] = 1;
+      *next_cell = 1;
+      cell1++;
+      cell2++;
+      cell3++;
+      next_cell++;
     }
     else
     {
-      next_arr[i] = 0;
+      *next_cell = 0;
+      cell1++;
+      cell2++;
+      cell3++;
+      next_cell++;
     }
   }
+  *next_cell = 0;
   return next_arr;
 }
 
