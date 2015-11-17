6fa060a97e229809e3c125eca814af119ff9578e
YongkyunLee <ykhl1itj@naver.com>
2015-11-16 20:53:09 -0800

    1d_cell_auto.c complete
    1D cellular automation using array method
    completed
    -completed/tested source code
    -fixed style errors
    -fixed comments

--- Assignment5_1d_cell_automata/1d_cell_auto.c
+++ Assignment5_1d_cell_automata/1d_cell_auto.c
@@ -6,7 +6,7 @@
 
 /* Signature Function */
 int *initial_array(int num_element);
-/*int update(int arr[], int num_element);*/
+int *update(int arr[], int num_element);
 void print_arr(int arr[], int num_element);
 
 /* This function generates the initial array of the 1d game of life.
@@ -14,7 +14,7 @@ The size of array is decided by the input integer.
 The first and last cell are set to be empty, and all the other cells are
 designated to be either full or empty randomly.
 Arguments:
- n: the number of cells in the array
+ num_element: the number of cells in the array
 Return value: randomly generated array of cells */
 int *initial_array(int num_element)
 {
@@ -26,7 +26,7 @@ int *initial_array(int num_element)
   /* Check that the calloc call succeeded */
   if (arr == NULL)
   {
-    fprintf(stderr, "Error! Memory allocation failed\n");
+    fprintf(stderr, "Error! Memory allocation failed.\n");
     exit(1);
   }
   arr[0] = 0; /* first and last elements are empty */
@@ -44,27 +44,29 @@ Arguments:
  arr[] : input array that is base of the next generation
  int num_element : the number of cells in arr[]
 Return value: the next generation produced from arr[] */
-/*
-int update(int arr[], int num_element)
+int *update(int arr[], int num_element)
 {
-  int *next_arr[num_element];
   int i;
-*/
-  /* The first and the last cells are always empty. */
-/*
-  next_arr[0] = 0;
-  next_arr[num_element-1] = 0;
-*/
-  /* make next generation array according to the rules:
-  Full(1) if one of adjacent cells if full and Empty(0) otherwise */
-/*
-  for (i=1; i<num_element-1; i++)
+  int *next_arr; /* array of next generation cells */
+  /* initialize array to have num_element elements */
+  next_arr = (int *)calloc(num_element, sizeof(int));
+  /* Check that the calloc call succeeded */
+  if (next_arr == NULL)
   {
-    if (arr[i]==0 && arr[i-1]==1 && arr[i+1]==0 )
+    fprintf(stderr, "Error! Memory allocation failed.\n");
+    exit(1);
+  }
+  next_arr[0] = 0;   /* The first and the last cells are always empty. */
+  next_arr[num_element - 1] = 0;
+  /* make next generation array according to the rules:
+  Full(1) if one of adjacent cells is full. Empty(0) otherwise */
+  for (i = 1; i < num_element - 1; i++)
+  {
+    if (arr[i] == 0 && arr[i - 1] == 1 && arr[i + 1] == 0)
     {
       next_arr[i] = 1;
     }
-    else if (arr[i]==0 && arr[i-1]==0 && arr[i+1]==1)
+    else if (arr[i] == 0  && arr[i - 1] == 0 && arr[i + 1]==1)
     {
       next_arr[i] = 1;
     }
@@ -75,18 +77,16 @@ int update(int arr[], int num_element)
   }
   return next_arr;
 }
-*/
 
 /* Prints the input array in a single row.
 Arguments:
  arr[]: the input array to print
  num_element: the number of cells in the input array
-Return value: None*/
-
+Return value: None */
 void print_arr(int arr[], int num_element)
 {
   int i;
-  for (i=0; i<num_element; i++)
+  for (i = 0; i < num_element; i++)
   {
     printf("%d", arr[i]);
   }
@@ -95,10 +95,34 @@ void print_arr(int arr[], int num_element)
 
 int main(int argc, char *argv[])
 {
-  /* initial array and free it */
-  int *arr_init;
+  int *arr1, *arr2;
+  int i;
   int num_element = atoi(argv[1]);
-  arr_init = initial_array(num_element);
-  print_arr(arr_init, num_element);
+  int num_generation = atoi(argv[2]);
+  arr1 = initial_array(num_element);
+  print_arr(arr1, num_element);
+  if (num_generation > 1) /* If there is 1 generation, loop does not work */
+  {
+    for (i = 1; i < num_generation; i++)
+    {
+      /* Alternate between arr1, arr2 to store the updated array */
+      if (i % 2 == 1)
+      {
+        arr2 = update(arr1, num_element);
+        print_arr(arr2, num_element);
+        free(arr1);
+      }
+      else
+      {
+        arr1 = update(arr2, num_element);
+        print_arr(arr1, num_element);
+        free(arr2);
+      }
+    }
+  }
+  else
+  {
+    free(arr1);
+  }
   return 0;
 }
